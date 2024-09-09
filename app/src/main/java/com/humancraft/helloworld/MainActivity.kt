c352bbaec1e2d9ce087265ef46c6945df8d43b0b
lucho9903 <lluisfgn99@gmail.com>
2024-09-09 13:56:46 -0500

    filas y columnas

--- app/src/main/java/com/humancraft/helloworld/MainActivity.kt
+++ app/src/main/java/com/humancraft/helloworld/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalMapOf
@@ -31,7 +41,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            app()
+            App()
 
         }
     }
@@ -39,39 +49,65 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun app() {
+fun App() {
+    var counter by rememberSaveable { mutableStateOf(value = 0) }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Green)
+            .background(Color.White)
+            .padding(16.dp)
     ) {
         item {
             Image(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(400.dp),
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = R.drawable.logoturi),
                 contentDescription = null
             )
+            Row (modifier = Modifier.padding(top = 8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_favorite_24),
+                    contentDescription = "like",
+                    modifier = Modifier.clickable { counter++ }
+                )
+                Text(text = counter.toString(), color = Color.Black, modifier = Modifier.padding(start = 4.dp), fontSize = 20.sp)
+            }
             Text(
-                text = "luis",
+                text = "Luis",
                 fontSize = 32.sp,
-                color = Color.White,
+                color = Color.Black,
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
-            Text(text = "fernando",
+            Text(
+                text = "fernando",
                 fontSize = 32.sp,
-                color = Color.White,
+                color = Color.Black,
                 modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center)
-            Text(text = "HOLA",
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "HOLA",
                 fontSize = 32.sp,
-                color = Color.White,
+                color = Color.Black,
                 modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center)
+                textAlign = TextAlign.Center
+            )
+            LazyRow(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                item {
+                    Text(
+                        text = "Estefa",
+                        fontSize = 32.sp,
+                        color = Color.Black
+                    )
+                }
+            }
         }
     }
-
-
 }
